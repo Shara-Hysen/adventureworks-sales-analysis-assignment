69d3ce5d119b794143af085f3e3d4918152da134
Shara Hysén <shara.hysen@outlook.com>
2025-12-22 12:05:37 +0100

    Add "analys 3"

--- Data/vis4.sql
+++ Data/vis4.sql
@@ -1,3 +1,4 @@
+USE AdventureWorks2025
 SELECT * FROM Sales.SalesOrderHeader
 
 SELECT
--- Data/vis5.sql
+++ Data/vis5.sql
@@ -1,3 +1,4 @@
+USE AdventureWorks2025
 SELECT * FROM Production.Product
 SELECT * FROM Sales.SalesOrderDetail
 
