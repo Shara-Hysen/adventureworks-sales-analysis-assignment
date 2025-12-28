a10ed7b99fcc7557500e046573b6fece94d39efb
Shara Hysén <shara.hysen@outlook.com>
2025-12-28 16:33:01 +0100

    Add Analys 6 and code for analys 7

--- Data/vis7.sql
+++ Data/vis7.sql
@@ -1,9 +1,10 @@
+USE AdventureWorks2025
 SELECT * FROM Sales.SalesTerritory
 SELECT * FROM Sales.SalesOrderHeader
 SELECT * FROM Sales.Customer
 SELECT * FROM Sales.Store
 
-
+-- Gjorde först en Query med AVG men insåg att det var bättre med råa aggregator, så använde inte denna
 SELECT
     st.Name AS RegionName,
     CASE 
@@ -17,3 +18,20 @@ INNER JOIN Sales.SalesOrderHeader soh ON c.CustomerID = soh.CustomerID
 GROUP BY st.Name, 
         CASE WHEN c.StoreID IS NOT NULL THEN 'Store' ELSE 'Person' END
 ORDER BY AvgOrderValue DESC
+
+
+-- Ny Query, bättre för att använda vidare med python i Notebook
+SELECT
+    st.Name AS RegionName,
+    CASE 
+        WHEN c.StoreID IS NOT NULL THEN 'Store'
+        ELSE 'Person'
+    END AS CustomType,
+    SUM(soh.SubTotal) AS TotalSales,
+    COUNT(*) AS OrderCount
+FROM Sales.SalesTerritory st  
+INNER JOIN Sales.Customer c ON st.TerritoryID = c.TerritoryID
+INNER JOIN Sales.SalesOrderHeader soh ON c.CustomerID = soh.CustomerID
+GROUP BY st.Name, 
+         CASE WHEN c.StoreID IS NOT NULL THEN 'Store' ELSE 'Person' END
+ORDER BY st.Name, CustomType DESC
